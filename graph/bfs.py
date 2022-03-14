aa3e2d2ca36c4e645def34a43ab0bcba40717029
Michael Moncada <michael@etherealrealmsstudios.com>
2022-03-14 10:17:43 -0400

    chore: Add BFS and DFS algorithm stubs

---
+++ graph/bfs.py
@@ -0,0 +1,6 @@
+from algorithm.Algorithm import Algorithm
+from .data_structure.undirected import UndirectedGraph
+
+
+class BreadthFirstSearch(Algorithm):
+    pass
---
+++ graph/dfs.py
@@ -0,0 +1,6 @@
+from algorithm.Algorithm import Algorithm
+from .data_structure.undirected import UndirectedGraph
+
+
+class DepthFirstSearch(Algorithm):
+    pass
